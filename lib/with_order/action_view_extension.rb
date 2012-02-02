a67e20bc3f99e00dbb5a2187bd1813dc3f540f07
Aaron Lasseigne <alasseigne@sei-mi.com>
2012-02-01 22:30:10 -0600

    renaming with_order_link to link_with_order and rework inputs to include scope

--- lib/with_order/action_view_extension.rb
+++ lib/with_order/action_view_extension.rb
@@ -1,20 +1,21 @@
 module WithOrder
   module ActionViewExtension
-    def with_order_link(*args, &block)
-      text = field = ''
-      html_options = {}
+    def link_with_order(*args, &block)
+      text = scope = field = html_options = nil
 
       if block_given?
         text         = capture(&block)
-        field        = args[0]
-        html_options = args[1] || {}
-      else
-        text         = args[0]
+        scope        = args[0]
         field        = args[1]
         html_options = args[2] || {}
+      else
+        text         = args[0]
+        scope        = args[1]
+        field        = args[2]
+        html_options = args[3] || {}
       end
 
-      dir = html_options.delete(:dir) || ((params[:order] == field.to_s and (params[:dir].blank? or params[:dir].downcase == 'asc')) ? 'desc' : 'asc')
+      dir = html_options.delete(:dir) || ((scope.current_order[:field] == field and (scope.current_order[:dir].blank? or scope.current_order[:dir].downcase == 'asc')) ? 'desc' : 'asc')
 
       link_to(text, params.merge({order: field, dir: dir}), html_options)
     end
--- spec/helpers/action_view_extension_spec.rb
+++ spec/helpers/action_view_extension_spec.rb
@@ -1,13 +1,14 @@
 require 'spec_helper'
 
 describe WithOrder::ActionViewExtension do
-  describe '#with_order_link(*args, &block)' do
+  describe '#link_with_order(*args, &block)' do
     context 'the current field ordered on is "first_name"' do
       it 'reverses the existing :dir if the field is "first_name"' do
         helper.stub!(:params).and_return({order: 'first_name', dir: 'asc'})
         npw = NobelPrizeWinner.with_order(helper.params)
-        helper.with_order_link(
+        helper.link_with_order(
           (npw.current_order[:field] == :first_name and npw.current_order[:dir] == 'asc') ? 'DESC' : 'ASC',
+          npw,
           :first_name
         ).should == '<a href="/assets?dir=desc&amp;order=first_name">DESC</a>'
       end
@@ -15,8 +16,9 @@ describe WithOrder::ActionViewExtension do
       it 'creates an "asc" order for fields other than "first_name"' do
         helper.stub!(:params).and_return({order: 'first_name', dir: 'asc'})
         npw = NobelPrizeWinner.with_order(helper.params)
-        helper.with_order_link(
+        helper.link_with_order(
           (npw.current_order[:field] == :last_name and npw.current_order[:dir] == 'asc') ? 'DESC' : 'ASC',
+          npw,
           :last_name
         ).should == '<a href="/assets?dir=asc&amp;order=last_name">ASC</a>'
       end
@@ -24,8 +26,9 @@ describe WithOrder::ActionViewExtension do
       it 'defaults to "desc" if no :dir param is given' do
         helper.stub!(:params).and_return({order: 'first_name'})
         npw = NobelPrizeWinner.with_order(helper.params)
-        helper.with_order_link(
+        helper.link_with_order(
           (npw.current_order[:field] == :first_name and npw.current_order[:dir] == 'asc') ? 'DESC' : 'ASC',
+          npw,
           :first_name
         ).should == '<a href="/assets?dir=desc&amp;order=first_name">DESC</a>'
       end
@@ -34,8 +37,10 @@ describe WithOrder::ActionViewExtension do
     context 'the :dir option is passed' do
       it 'uses the provided :dir option as the param value' do
         helper.stub!(:params).and_return({order: 'first_name', dir: 'asc'})
-        helper.with_order_link(
+        npw = NobelPrizeWinner.with_order(helper.params)
+        helper.link_with_order(
           'ASC',
+          npw,
           :first_name,
           {dir: 'asc'}
         ).should == '<a href="/assets?dir=asc&amp;order=first_name">ASC</a>'
@@ -44,8 +49,10 @@ describe WithOrder::ActionViewExtension do
 
     it 'accepts #link_to options' do
       helper.stub!(:params).and_return({order: 'first_name', dir: 'asc'})
-      helper.with_order_link(
+      npw = NobelPrizeWinner.with_order(helper.params)
+      helper.link_with_order(
         'ASC',
+        npw,
         :first_name,
         {dir: 'asc', remote: true}
       ).should == '<a href="/assets?dir=asc&amp;order=first_name" data-remote="true">ASC</a>'
@@ -54,7 +61,7 @@ describe WithOrder::ActionViewExtension do
     it 'accepts blocks' do
       helper.stub!(:params).and_return({order: 'first_name', dir: 'asc'})
       npw = NobelPrizeWinner.with_order(helper.params)
-      output = helper.with_order_link(:first_name, {remote: true}) do
+      output = helper.link_with_order(npw, :first_name, {remote: true}) do
         (npw.current_order[:field] == :first_name and npw.current_order[:dir] == 'asc') ? 'DESC' : 'ASC'
       end
       output.should == '<a href="/assets?dir=desc&amp;order=first_name" data-remote="true">DESC</a>'
