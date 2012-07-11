3397475ff879370479168e772f6c2cd52ce7b0c9
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2012-07-10 22:34:40 -0500

    minor refactoring

--- lib/with_order/action_view_extension.rb
+++ lib/with_order/action_view_extension.rb
@@ -3,19 +3,9 @@ module WithOrder
     include WithOrder::HashExtraction
 
     def link_with_order(*args, &block)
-      text = scope = field = html_options = nil
-
-      if block_given?
-        text         = capture(&block)
-        scope        = args[0]
-        field        = args[1]
-        html_options = args[2] || {}
-      else
-        text         = args[0]
-        scope        = args[1]
-        field        = args[2]
-        html_options = args[3] || {}
-      end
+      text = block_given? ? capture(&block) : args.shift
+      scope, field, html_options = *args
+      html_options ||= {}
 
       dir = html_options.delete(:dir) || (
         (scope.current_order[:field] == field and (scope.current_order[:dir].blank? or scope.current_order[:dir].downcase == :asc)) ?
